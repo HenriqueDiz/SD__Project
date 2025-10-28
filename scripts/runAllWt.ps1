819a048d4ae6fc62a69b215448b5784d12740671
henrique.diz.10 <henrique.diz.10@gmail.com>
2025-10-28 01:16:57 +0000

    intervalos entre terminais

--- scripts/runAllWt.ps1
+++ scripts/runAllWt.ps1
@@ -5,18 +5,14 @@ if (-not (Get-Command wt -ErrorAction SilentlyContinue)) {
     exit 1
 }
 
-# Escapa aspas no caminho
-$escaped = $cwd -replace '"','\"'
+# Usar aspas duplas ao redor do caminho completo
+$escapedPath = "`"$cwd`""
 
 # Comando que cria 3 terminais cada um com 2 painéis (o primeiro com URL Queue e o Barrel 1, o segundo com o Barrel 2 e o Gateway, e o terceiro com o Client e o Downloader)
-$cmd = "new-tab cmd /k `"cd /d $escaped && title url-queue && make run-q`";"
-$cmd += " split-pane -V cmd /k `"cd /d $escaped && title barrel1 && make run-b1`";"
-
-
-$cmd += " ; new-tab cmd /k `"cd /d $escaped && title barrel2 && make run-b2`";"
-$cmd += " split-pane -V cmd /k `"cd /d $escaped && title gateway && make run-g`";"
-
-$cmd += " ; new-tab cmd /k `"cd /d $escaped && title client && make run-c`";"
-$cmd += " split-pane -V cmd /k `"cd /d $escaped && title downloader && make run-d`""
-
-Start-Process wt -ArgumentList $cmd
+wt `
+  new-tab cmd /k "cd /d $escapedPath && title url-queue && make run-q" `; `
+  split-pane -V cmd /k "cd /d $escapedPath && title barrel1 && make run-b1" `; `
+  new-tab cmd /k "cd /d $escapedPath && title barrel2 && make run-b2" `; `
+  split-pane -V cmd /k "cd /d $escapedPath && title gateway && timeout /t 2 /nobreak && make run-g" `; `
+  new-tab cmd /k "cd /d $escapedPath && title client && timeout /t 5 /nobreak && make run-c" `; `
+  split-pane -V cmd /k "cd /d $escapedPath && title downloader && timeout /t 5 /nobreak && make run-d"
